e65d5e29a7c5155564ce7f264e4f0a1a310bfdee
camibvir <virginiabaigorria005@gmail.com>
2025-04-01 14:44:16 -0300

    4)c

--- tp1_2.c
+++ tp1_2.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 #include <math.h>
 void cuadradoNumeroVacio (int numero);
+void mostrarVariable (int variable);
 
 int main (){
-    int numero=3;
+    int numero=3, variable=2;
     cuadradoNumeroVacio(numero);
+    mostrarVariable(variable);
     
 }
 
 void cuadradoNumeroVacio (int numero) {
     int cuadrado = numero*numero;
     printf("b. El cuadrado del numero 3 = %d\n", cuadrado);
+}
+
+void mostrarVariable(int variable){
+    printf("c. El valor de la variable es: %d y su direccion de memoria es: %d\n", variable, &variable);
 }
