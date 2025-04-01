2e658335853290c323e61a77f88c8081602618b9
camibvir <virginiabaigorria005@gmail.com>
2025-04-01 14:17:27 -0300

    commit punto 2e

--- tp1_1.c
+++ tp1_1.c
@@ -2,5 +2,17 @@
 
 int main (){
 
-    printf ("Hola mundo");
+    printf ("Hola mundo\n");
+
+    int variable, *puntero;
+    variable = 10;
+    puntero = &variable;
+    printf("El valor del contenido del puntero es: %d\n", *puntero);
+    printf("La dirección de memoria almacenada por el puntero es: %d\n", puntero);
+    printf("La dirección de memoria de la variable es: %d\n ",&variable);
+    printf("La dirección de memoria del puntero es: %d\n", &puntero);
+    printf("El tamaño de memoria utilizado por la variable es: %d\n", sizeof(variable));
+
+    
+
 }
